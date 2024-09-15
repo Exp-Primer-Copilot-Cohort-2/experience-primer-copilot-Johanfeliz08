1c69c09fce75eb3c38e124a84f3f54a3677cccd3
Johan E. Feliz Ureña <76084611+Johanfeliz08@users.noreply.github.com>
2024-09-15 01:21:50 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+  return {
+    skills: [
+      { name: "HTML", level: 5 },
+      { name: "CSS", level: 5 },
+      { name: "JavaScript", level: 5 },
+      { name: "React", level: 5 },
+      { name: "Node.js", level: 5 },
+      { name: "MongoDB", level: 5 },
+    ],
+  };
+}
